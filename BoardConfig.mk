fbfbc2e3005a66d09d44aed182e3dfc68e47e8b9
Alin Jerpelea <jerpelea@gmail.com>
2012-11-15 20:15:28 +0200

    lotus: bootimg cleanup
    Change-Id: I4e128388bd218dd90c2964c6ba3aff878b56e924

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,19 +6,14 @@ USE_CAMERA_STUB := true
 #inherit from the common montblanc definitions
 -include device/sony/montblanc-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_SOURCE := kernel/sony/u8500
 TARGET_KERNEL_CONFIG := cyanogen_lotus_defconfig
-#TARGET_PREBUILT_KERNEL := device/sony/lotus/prebuilt/kernel
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
 
-# the following two sizes are generous guesses
-# since these partitions are not visible
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_CMDLINE := device/sony/montblanc-common/config/cmdline_512M.txt
+BOARD_CMDLINE := device/sony/lotus/config/cmdline.txt
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
 BOARD_USERDATA_PARTITION_SIZE := 2147483648
