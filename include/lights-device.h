c5cfc2831ba98d3dc503b67b00d2a13232828995
Alin Jerpelea <jerpelea@gmail.com>
2013-03-09 08:10:10 +0200

    lotus: lights update
    Change-Id: I5bc0e8a8ff96c91e06cfd3bff86daba46abcd95a

--- include/lights-device.h
+++ include/lights-device.h
@@ -1,19 +1,12 @@
-char const*const RED_LED_FILE[] = {
- "/sys/class/leds/red/brightness",
-};
-char const*const GREEN_LED_FILE[]= {
- "/sys/class/leds/green/brightness",
-};
-char const*const BLUE_LED_FILE[] = {
- "/sys/class/leds/blue/brightness",
-};
+char const*const RED_LED_FILE = "/sys/class/leds/red/brightness";
+char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
+char const*const BLUE_LED_FILE = "/sys/class/leds/blue/brightness";
 
 char const*const LED_FILE_TRIGGER[] = {
  "/sys/class/leds/red/use_pattern",
  "/sys/class/leds/green/use_pattern",
  "/sys/class/leds/blue/use_pattern",
 };
-
 
 char const*const BUTTON_BACKLIGHT_FILE[] = {
  "/sys/class/leds/button-backlight/brightness",
