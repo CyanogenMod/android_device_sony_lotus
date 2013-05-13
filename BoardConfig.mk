2179a6e392524e2cf327e5992dd162d012a45a64
Alin Jerpelea <jerpelea@gmail.com>
2013-05-13 18:48:30 +0300

    lotus: rename cm_defconfig
    Change-Id: I8eb9db3cfd155e038048cf8f0ff7c1fb9f1467a8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ USE_CAMERA_STUB := true
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/lotus/include
 
-TARGET_KERNEL_CONFIG := cyanogen_lotus_defconfig
+TARGET_KERNEL_CONFIG := cm_montblanc_lotus_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
